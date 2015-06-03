ef55bde56b5db1ed78d737b223d62c78a9bbb88b
Adrian Mugnolo <adrian@wyeworks.com>
2015-06-03 14:05:10 -0300

    Make code prettier
    According to http://jsbeautifier.org/.

--- chronoarchive.js
+++ chronoarchive.js
@@ -1,48 +1,42 @@
-var addressesAndExpiration = [
-  {
-    address : "messages-noreply@linkedin.com",
-    days : 3
-  },
-  {
-    address : "no-reply@slack.com",
-    days : 5
-  },
-  {
-    address : "info@twitter.com",
-    days : 5
-  },
-  {
-    address : "info@meetup.com",
-    days : 5
-  },
-  {
-    address : "digest-noreply@quora.com",
-    days : 7
-  }
-];
+var addressesAndExpiration = [{
+  address: "messages-noreply@linkedin.com",
+  days: 3
+}, {
+  address: "no-reply@slack.com",
+  days: 5
+}, {
+  address: "info@twitter.com",
+  days: 5
+}, {
+  address: "info@meetup.com",
+  days: 5
+}, {
+  address: "digest-noreply@quora.com",
+  days: 7
+}];
 
 var MILIS_PER_DAY = 1000 * 60 * 60 * 24;
 var nowDate = new Date();
 
-function isMailContained(sample, mail){
-  return (function(sample, mail){
+function isMailContained(sample, mail) {
+  return (function(sample, mail) {
     this.regexCache = this.regexCache || {};
-    if(this.regexCache[mail] === undefined){
+    if (this.regexCache[mail] === undefined) {
       this.regexCache[mail] = new RegExp(mail);
     }
     return this.regexCache[mail].test(sample);
   })(sample, mail);
 }
 
-function haveNDaysPassed(days, date){
+function haveNDaysPassed(days, date) {
   return date.getTime() + days * MILIS_PER_DAY <= nowDate.getTime();
 }
 
-function isOld(message){
+function isOld(message) {
   var from = message.getFrom();
   var date = message.getDate();
-  for(var i = 0, j = addressesAndExpiration.length; i < j; i++){
-    if(isMailContained(from, addressesAndExpiration[i].address)){
+  for (var i = 0, j = addressesAndExpiration.length; i < j; i++) {
+    if (isMailContained(from, addressesAndExpiration[i].address)) {
       return haveNDaysPassed(addressesAndExpiration[i].days, date);
     }
   }
@@ -50,9 +44,9 @@ function isOld(message){
 
 function checkOldEphemeralMail() {
   var inbox = GmailApp.getInboxThreads();
-  for(var i = 0, j = inbox.length; i < j; i++){
+  for (var i = 0, j = inbox.length; i < j; i++) {
     var message = inbox[i].getMessages()[0];
-    if(isOld(message)){
+    if (isOld(message)) {
       inbox[i].moveToArchive();
     }
   }
